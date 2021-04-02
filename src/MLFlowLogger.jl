d828a56ea3c92a64653f81a4fa1a928c6da784ef
rejuvyesh <mail@rejuvyesh.com>
2021-04-02 20:59:24 +0000

    copy over the filename to output as well for logging images

--- src/MLFlowLogger.jl
+++ src/MLFlowLogger.jl
@@ -106,6 +106,7 @@ function log_image(lg::MLFLogger, key::AbstractString, obj::AbstractString; step
     if !isnothing(step)
         key = key * "/$step"
     end
+    key = key * "/" basename(obj)
     log_artifact(lg, obj, key)
 end
 
